cb923419f9fbf857a1ac01b4b00cc938b05909f0
campeonatogranturismo5-ui <campeonatogranturismo5@gmail.com>
2026-05-23 19:49:09 +0200

    Add Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6366f1",
+      },
+    },
+  },
+} satisfies Config;
